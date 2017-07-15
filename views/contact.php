58b69527342f95e2a0535b5606bf69063e7d4f4b
Andy Vanellus <andrejcibik@gmail.com>
2017-07-16 01:44:42 +0200

    Contact fixes

--- views/contact.php
+++ views/contact.php
@@ -109,7 +109,7 @@
 	<a href="https://www.behance.net/AndrejCibik" class="button rippleHover icon-behance" title="Behnace" target="_blank"></a>
 	<a href="https://dribbble.com/AndrejCibik" class="button rippleHover icon-dribbble" title="Dribbble" target="_blank"></a>
 	<a href="https://www.facebook.com/AndrejCibikDesign/" class="button rippleHover icon-facebook" title="FB page"></a>
-	<a href="https://www.instagram.com/andrejcibik/" class="button rippleHover icon-instagram" title="Instagram" target="_blank"></a>
+	<a href="https://www.instagram.com/andrejcibik_design/" class="button rippleHover icon-instagram" title="Instagram" target="_blank"></a>
 	<a href="https://www.youtube.com/channel/UCwVEiFeuccoAzgwpAsSl7Tw" class="button rippleHover icon-youtube" title="Youtube" target="_blank"></a>
 	<a href="mailto:hello@andrejcibik.sk" class="button rippleHover icon-mail" title="Email" ></a>
 
